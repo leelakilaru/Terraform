11f3b067cfaacaa3d4682edaa795f60f94c406b0
leelakilaru <aneela.kilaru@gmail.com>
2024-10-18 21:00:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "first" {
   ami           = "ami-0ddc798b3f1a5117e"
   instance_type = "t2.micro"
   tags = {
-    Name = " second_Jenkins_Server"
+    Name = " third_Jenkins_Server"
   }
 }
