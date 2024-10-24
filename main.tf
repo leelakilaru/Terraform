8bf25e0acb98b5159c584e526a4d9940d9e0b3f4
leelakilaru <aneela.kilaru@gmail.com>
2024-10-24 10:36:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "Newkey"
+vpc_security_group_ids = ["sg-0a772c42ba1b50435"]
 tags = {
 Name = var.instance_names[count.index]
 }
